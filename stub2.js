f492536e81530c61c57353c00ef90e05ce1664a2
Rodrigo Ehlers <rodrigoehlers@outlook.com>
2019-12-09 12:35:33 +0100

    switch stubs

--- stub2.js
+++ stub2.js
@@ -1,6 +1,17 @@
-const a = '1';
-const b = '2';
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const c = a + b;
-
-console.log(c);
+readline.question(`What's your name?\n`, name => {
+  console.log(`Hi ${name}!`);
+  readline.question(`How old will you be by the end of the year?\n`, age => {
+    const num = Number.parseInt(age);
+    if (Number.isNaN(num)) {
+      console.log('That is not a real age.. :(');
+    } else {
+      console.log(`So you were born in ${2019 - num}.`);
+    }
+    readline.close();
+  });
+});
